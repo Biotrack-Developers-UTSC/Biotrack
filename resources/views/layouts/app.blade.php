50e87d74578318726f8dd02fe33d03ea43fc4cda
PedroAlbertoPM <pedroperezpm2@gmail.com>
2025-11-06 17:49:36 -0600

    Commit: Mejora de vistas para moviles.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <title>@yield('title', 'BioTrack') - Seguimiento de Especies</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
